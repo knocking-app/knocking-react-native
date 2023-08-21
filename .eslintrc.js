4f5130572e994180cfcfc3c0c2b7fb2af25fb8f1
kgkurbanov <kgkurbanov@sbertech.ru>
2023-08-21 15:29:39 +0300

    knoking: added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    root: true,
+    extends: ['universe/native'],
+    rules: {
+        'newline-before-return': 'error',
+        'no-unused-vars': 'off',
+        'no-dupe-else-if': 'error',
+        'no-duplicate-imports': 'error',
+        camelcase: 'error',
+        'no-console': 'error',
+        'no-magic-numbers': 'error',
+        'prefer-const': 'error',
+        'arrow-spacing': [
+            'error',
+            {
+                before: true,
+                after: true,
+            },
+        ],
+        'default-case': 'error',
+        'max-params': ['error', 3],
+        '@typescript-eslint/no-unused-vars': 'error',
+        'linebreak-style': ['error', 'unix'],
+        quotes: [
+            'error',
+            'single',
+            {
+                avoidEscape: true,
+            },
+        ],
+        semi: ['error', 'never'],
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/no-children-prop': 'error',
+        'react/function-component-definition': 'error',
+        'react/jsx-boolean-value': ['error', 'never'],
+        'react-hooks/exhaustive-deps': 'error',
+        'prettier/prettier': 'error',
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+                alphabetize: {
+                    order: 'asc',
+                },
+                'newlines-between': 'always',
+            },
+        ],
+    },
+}
