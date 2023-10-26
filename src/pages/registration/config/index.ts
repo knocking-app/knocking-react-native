52f87c79c17ef77d8ae38352ffd96e8e470f18ec
kgkurbanov <kgkurbanov@sbertech.ru>
2023-10-26 11:47:28 +0300

    Добавит поле верификации для страницы регистрации

--- src/pages/registration/config/index.ts
+++ src/pages/registration/config/index.ts
@@ -1,9 +1,10 @@
-import { object, string, } from 'yup'
+import { object, string, ref} from 'yup'
 
 const REQUAIRED_MSG = 'Обязательно для заполнения'
 
 export const validationSchema = object().shape({
     login: string().required(REQUAIRED_MSG),
     password: string().required(REQUAIRED_MSG),
-    email: string().required(REQUAIRED_MSG).email('не корретный email')
+    email: string().required(REQUAIRED_MSG).email('не корретный email'),
+    verification: string().oneOf([ref('password')], 'Passwords must match')
 })
